bee2b789a1d7f0f1762c88ec9de9b66717919397
ravialiaswire <ravikumar.7oct@gmail.com>
2015-09-30 12:40:44 +0530

    my first git commit

---
+++ src/wire/Sample.java
@@ -0,0 +1,13 @@
+package wire;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Test test1 = new Test();
+		test1.display();
+		System.out.println("this is sample java sample class");
+	}
+
+}
---
+++ src/wire/Test.java
@@ -0,0 +1,10 @@
+package wire;
+
+public class Test {
+	
+	public void display()
+	{
+		System.out.println("my java test class");
+	}
+
+}
